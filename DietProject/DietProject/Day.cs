dd10ce8937634523396120a6a12cc345e17146b9
redista <sealacquire@gmail.com>
2021-04-27 20:57:51 +0100

    added basic xaml and some func

---
+++ DietProject/DietProject/Day.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietProject
+{
+    class Day
+    {
+        public int Id { get; set; }
+        public string date { get; set; }
+        public List<Meal> Meals { get; set; }
+    }
+
+    class Meal
+    {
+        public string Name { get; set; }
+        public string Time { get; set; }
+        public int Calories { get; set; }
+        public int Protein { get; set; }
+        public int Carbs { get; set; }
+        public int Fat { get; set; }
+    }
+}
--- DietProject/DietProject/MainWindow.xaml.cs
+++ DietProject/DietProject/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiteDB;
 
 namespace DietProject
 {
@@ -20,6 +21,10 @@ namespace DietProject
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        LiteDatabase db = new LiteDatabase(@"C:\db\userData.db");
+        ILiteCollection<Day> dbDays;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,67 +32,53 @@ namespace DietProject
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Meals = new List<Meal>();
-            try
-            {
-                using (StreamReader sw = new StreamReader(@"C:\Users\vkqwn\Documents\college\y2\ood\project\DietApp\DietApp\data\Meals.json", Encoding.UTF8))
-                {
-                    string jsonData = sw.ReadToEnd().ToString();
+            dbDays = db.GetCollection<Day>("days");
 
-                    MessageBox.Show(jsonData);
-                    Days = JsonSerializer.Deserialize<DaysData>(jsonData);
-                }
-            }
-            catch (Exception err)
-            {
-                MessageBox.Show(err.ToString());
-            }
-
-            MealsDate_tbk.Text = String.Format($"Today - {DateTime.Now.ToString("dd/MM/yyyy")}");
-
-            foreach (Day day in Days.Days)
-            {
-                Days_cbx.Items.Add(day);
-            }
-
-
+            RefreshDays();
         }
 
         private void AddMeal_btn_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                Days.Days[Days_cbx.SelectedIndex].Meals.Add(new Meal(MealName_tbx.Text,      // Name
-                                   Int32.Parse(MealCalories_tbx.Text),                       // Calories
-                                   Int32.Parse(MealCarb_tbx.Text),                           // Carbs
-                                   Int32.Parse(MealProtein_tbx.Text),                        // Protein
-                                   Int32.Parse(MealFat_tbx.Text)));                          // Fat
+            //if()
+            //try
+            //{
+            //    dbDays.Insert[Days_cbx.SelectedIndex].Meals.Add(new Meal(
+            //        { MealName_tbx.Text,      // Name
+            //                       Int32.Parse(MealCalories_tbx.Text),                       // Calories
+            //                       Int32.Parse(MealCarb_tbx.Text),                           // Carbs
+            //                       Int32.Parse(MealProtein_tbx.Text),                        // Protein
+            //                       Int32.Parse(MealFat_tbx.Text)));                          // Fat
 
-                Meals_lbx.ItemsSource = null;
-                Meals_lbx.ItemsSource = Days.Days[Days_cbx.SelectedIndex].Meals;
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Error");
-            }
-        }
+            //    Meals_lbx.ItemsSource = null;
+            //    Meals_lbx.ItemsSource = Days.Days[Days_cbx.SelectedIndex].Meals;
+            //}
+            //catch (Exception)
+            //{
+        //    messagebox.show("error");
+        //}
+    }
 
         private void Meals_lbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Meal meal = Meals_lbx.SelectedItem as Meal;
-            if (meal != null)
+
+        }
+
+        private void Calender_lbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Days_lbx.SelectedItem != null)
             {
-                MealName_tbx.Text = meal.Name;
-                MealCalories_tbx.Text = meal.Calories.ToString();
-                MealCarb_tbx.Text = meal.Carbs.ToString();
-                MealProtein_tbx.Text = meal.Protein.ToString();
-                MealFat_tbx.Text = meal.Fat.ToString();
+                Day day = Days_lbx.SelectedItem as Day;
+                Meals_lbx.ItemsSource = day.Meals; 
             }
         }
 
-        private void Days_cbx_DropDownClosed(object sender, EventArgs e)
+        private void RefreshDays()
         {
-            Meals_lbx.ItemsSource = Days.Days[Days_cbx.SelectedIndex].Meals;
+            var getDays = dbDays.Query()
+                        .Select(x => x.date)
+                        .ToList();
+
+            Days_lbx.ItemsSource = getDays;
         }
     }
 }
